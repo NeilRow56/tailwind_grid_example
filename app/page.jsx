d88b2a919da9e8a45528e6ada36cd4470b4f43c0
Neil <neilrowland56@yahoo.com>
2022-11-27 09:45:49 +0000

    first commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,57 +1,34 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import React from "react";
 
-export default function Home() {
+const Home = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
+    <div className="grid gap-4 max-w-5xl p-4 sm:grid-cols-2 md:grid-cols-4 ">
+      <h1 className="text-4xl font-extrabold sm:col-span-2  sm:grid sm:grid-cols-2 sm:gap-4 md:grid-cols-3 md:col-span-3 md:text-5xl ">
+        <span className="bg-yellow-200 md:col-span-2 ">
+          Grid Layout with Tailwind css
+        </span>
+      </h1>
+      <p className="sm:row-start-2 sm:col-start-2 self-center md:col-start-1 md:col-span-2 md:pr-12 md:text-lg">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nostrum, in?
+        Eaque aperiam ipsum quas cumque, voluptatem aliquid laudantium maxime
+        similique praesentium. Repudiandae vel ratione, et fugit placeat nihil
+        incidunt minima.
+      </p>
+      <div className="bg-blue-500 h-auto sm:square"></div>
+      <div className="bg-blue-500 h-auto sm:square"></div>
+      <div className="bg-pink-500 h-auto sm:square"></div>
+      <div className="bg-blue-500 h-auto sm:square md:col-start-2"></div>
+      <div className="bg-pink-500 h-auto sm:square"></div>
+      <div className="bg-blue-500 h-auto sm:square"></div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <div className="bg-blue-500 h-auto sm:square"></div>
+      <div className="bg-pink-500 h-auto sm:square"></div>
+      <p className="self-center md:text-lg md:col-span-2 md:text-center md:px-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae
+        architecto incidunt aperiam nam cum rerum.
+      </p>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
